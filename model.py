d7543118758f9e0bcc3bf3c638073977e2f4bed0
David <david.yang026@gmail.com>
2019-05-04 14:40:55 -0700

    first predictions for the model

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 
 
 
 def create_label(df):
-
-
 
 
     #add a new column called "Fraud"
@@ -17,4 +17,19 @@ def create_label(df):
     #this will be our target
     label = df["Fraud"]
 
-    return df
+    return df, label
+
+def prediction(df, features):
+
+    df, y = create_label(df)
+
+    X = df[features]
+
+    #split our data into train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33)
+
+    clf = RandomForestClassifier()
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    return predictions
